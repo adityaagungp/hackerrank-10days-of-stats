5d8015afaec7211a41a7c7dda7e1fc64ef53e694
adityaagungp <adityaagungputra@gmail.com>
2018-05-05 18:11:24 +0700

    day 8 answer

--- day8/day8-regression.py
+++ day8/day8-regression.py
@@ -11,18 +11,22 @@ def stdev(array):
         sq_dist += (i - arr_mean) ** 2
     return (sq_dist / len(array)) ** 0.5
 
-n = int(input())
-x_data = list(map(float, input().split()))
-y_data = list(map(float, input().split()))
+# hard-coded inputs
+x_data = [95, 85, 80, 70, 60]
+y_data = [85, 95, 70, 65, 70]
 
 # calculate covariant
 sum_var = 0
 mean_x = mean(x_data)
 mean_y = mean(y_data)
-for i in range(0, n):
+for i in range(0, 5):
     sum_var += (x_data[i] - mean_x) * (y_data[i] - mean_y)
-covariant = sum_var / n
+covariant = sum_var / 5
 
-# calculate PCC
-pcc = covariant / (stdev(x_data) * stdev(y_data))
-print(round(pcc, 3))
+# calculate a and b so that Y = a + bX
+b = covariant / (stdev(x_data) ** 2)
+a = mean_y - (b * mean_x)
+
+# expect Y if X = 80
+score = a + (b * 80)
+print(round(score, 3))
